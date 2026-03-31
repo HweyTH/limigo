4be452e408b6852a8451b04908ac1237666c68b0
Gia Huy Thai <thhuy1101@gmail.com>
2026-03-31 17:47:01 +0700

    add: allow function to access request to obtain token

--- internal/limiter/limiter.go
+++ internal/limiter/limiter.go
@@ -16,3 +16,19 @@ type TokenBucket struct {
 	rate       float64
 	lastRefill time.Time
 }
+
+func (bucket *TokenBucket) Allow(ctx context.Context, key string) (bool, error) {
+	elapsed := time.Since(bucket.lastRefill).Seconds()
+	numAddTokens := elapsed * bucket.rate
+	if bucket.count+numAddTokens < bucket.capacity {
+		bucket.count += numAddTokens
+	} else {
+		bucket.count = bucket.capacity
+	}
+	bucket.lastRefill = time.Now()
+	if bucket.count > 0 {
+		bucket.count -= 1
+		return true, nil
+	}
+	return false, nil
+}
